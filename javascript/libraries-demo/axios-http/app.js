effd2fd5735a8162781ff8be7d2b821c6b434192
Vijay Sarin <vijaysarin@live.com>
2016-04-12 13:19:22 +0530

    Axios Implementation with BlueBird Promise

--- javascript/libraries-demo/axios-http/app.js
+++ javascript/libraries-demo/axios-http/app.js
@@ -1,29 +1,39 @@
+// request prerequisites
 var requestConfig = {
     method: 'get',
     timeout: 600000,
-    headers: {'X-Custom-Header': 'foobar'},
+    headers: {
+        'X-Custom-Header': 'foobar'
+    },
     responseType: 'json'
 };
 
+// setting BlueBird promise as cancellable 
 Promise.config({
     cancellation: true
 });
 
-function callToServer(){
-    var promise = axios.get('http://graph.facebook.com/vijaysarin', null, requestConfig).then(function(response) {
+// hold every request
+var _axiosAjaxRequests = [];
+
+// axios call - sending the request
+function callToServer() {
+    if (_axiosAjaxRequests.length > 0) {
+        var lastPromise = _axiosAjaxRequests[0];
+        _axiosAjaxRequests = [];
+        lastPromise.cancel();
+    }
+    var promise = axios.get('/data/test-data.json', null, requestConfig).then(function (response) {
         console.log(response.status + " : " + response.statusText);
     }).catch(function (response) {
         if (response instanceof Error) {
-          // Something happened in setting up the request that triggered an Error
-          console.log('Error', response.message);
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', response.message);
         } else {
-          // The request was made, but the server responded with a status code
-          // that falls out of the range of 2xx
-          console.log(response.status + " : " + response.statusText);
+            // The request was made, but the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(response.status + " : " + response.statusText);
         }
     });
-    setTimeout(function(){
-        promise.cancel();    
-    }, 0);    
+    _axiosAjaxRequests.push(promise);
 }
-
